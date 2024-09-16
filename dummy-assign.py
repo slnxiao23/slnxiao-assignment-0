2f6de7f9573998991916b3e715186925111d4892
slnxiao23 <144143764+slnxiao23@users.noreply.github.com>
2024-09-16 08:22:04 +0800

    Add files via upload

---
+++ dummy-assign.py
@@ -0,0 +1,13 @@
+
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+if __name__ == "__main__":
+    # Example numbers
+    number1 = 10
+    number2 = 15
+    result = add_numbers(number1, number2)
+    
+    # Print the result
+    print(f"The sum of {number1} and {number2} is {result}")
